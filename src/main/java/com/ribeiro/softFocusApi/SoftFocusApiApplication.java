a5532daaf5aaaa41a65ebbd0d5ea0a57d63535e5
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-02-06 16:02:10 -0300

    Project Started

---
+++ src/main/java/com/ribeiro/softFocusApi/SoftFocusApiApplication.java
@@ -0,0 +1,13 @@
+package com.ribeiro.softFocusApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftFocusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftFocusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ribeiro/softFocusApi/SoftFocusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribeiro.softFocusApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftFocusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
